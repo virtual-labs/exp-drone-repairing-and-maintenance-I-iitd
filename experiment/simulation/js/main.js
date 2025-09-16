9f53003e0a89f5dda88a9ce5b45a3b3a321a86a6
Sneha Jadon <71967438+vlabsdev-iitd@users.noreply.github.com>
2025-09-17 00:43:59 +0530

    DE1 spinner-lazy loading added

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,37 +1,68 @@
-import { Download, Scenes, Src, Dom, DomList, Util, DB, DeveloperTools } from "./Libs.js";
+import { Download, Scenes, Src, Dom, DomList, Util, DB, DeveloperTools} from "./Libs.js";
 
+Download.spinner(true)
 
-document.addEventListener('DOMContentLoaded', ()=>{
+lazyLoading()
+
+window.addEventListener('load', ()=>{
+  Run()
+});
+
+function lazyLoading(){
+  let images = document.querySelectorAll("img")
+  images.forEach(img=>{
+    img.loading = "lazy"
+    img.decoding = "asynchronous"
+  })
+}
+
+function Run(){
+  // Scenes.steps = Steps.steps
+  
   // stepcalling
   Scenes.currentStep = 0;
   Scenes.next();
 
   Download.init()
-  // DeveloperTools.init()
+  Download.spinner(false)
 
-  $( function() {
-    // $( "img" ).draggable({
-    //   drag: function(event, ui){
-    //     console.log(ui.position.left, ui.position.top)
-    //   }
-    // });
-    // $( ".temp" ).draggable();
-    // $( ".video-box" ).draggable();
-    // $( ".modal-box" ).draggable();
-  } );
-});
+  // DeveloperTools.updateMousePosition()
+}
 
-// setTimeout(() => {
-// $(".main-container").hide();
-// }, 100);
 
-// $(document).ready(function () {
-//   // TODO uncomment
-//   Download.init();
-//   // Download.toggleSpinner()
 
-//   window.onbeforeprint = () => {
-//     Dom.setBlinkArrowRed(-1);
-//     Dom.setBlinkArrow(-1);
-//   };
+
+// document.addEventListener('DOMContentLoaded', ()=>{
+//   // stepcalling
+//   Scenes.currentStep = 0;
+//   Scenes.next();
+
+//   Download.init()
+//   // DeveloperTools.init()
+
+//   $( function() {
+//     // $( "img" ).draggable({
+//     //   drag: function(event, ui){
+//     //     console.log(ui.position.left, ui.position.top)
+//     //   }
+//     // });
+//     // $( ".temp" ).draggable();
+//     // $( ".video-box" ).draggable();
+//     // $( ".modal-box" ).draggable();
+//   } );
 // });
+
+// // setTimeout(() => {
+// // $(".main-container").hide();
+// // }, 100);
+
+// // $(document).ready(function () {
+// //   // TODO uncomment
+// //   Download.init();
+// //   // Download.toggleSpinner()
+
+// //   window.onbeforeprint = () => {
+// //     Dom.setBlinkArrowRed(-1);
+// //     Dom.setBlinkArrow(-1);
+// //   };
+// // });
